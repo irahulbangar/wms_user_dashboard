ed1550302a60ecbffa022dea56a1c2510f6c43ca
Rahul Bangar <rahul@bulfro.com>
2025-12-30 15:10:12 +0530

    Update SidebarMenuResult interface to include additional properties for enhanced functionality
    - Added new properties: show_plant, show_department, show_system, show_device, show_water_report, show_plant_layout, and show_notification to the SidebarMenuResult interface.
    - Ensured consistent formatting by adding semicolons to existing properties for improved code clarity.

--- model/sidebar-menu.interface.ts
+++ model/sidebar-menu.interface.ts
@@ -5,14 +5,21 @@ export interface SidebarMenuResponse {
 }
 
 export interface SidebarMenuResult {
-  plant_name: string
-  plant_id: number
-  address: string
-  department_name: string
-  department_id: number
-  system_name: string
-  system_id: number
-  organization_name: string
-  organization_id: number
-  subdomain: string
+  plant_name: string;
+  plant_id: number;
+  address: string;
+  department_name: string;
+  department_id: number;
+  system_name: string;
+  system_id: number;
+  organization_name: string;
+  organization_id: number;
+  subdomain: string;
+  show_plant: string;
+  show_department: string;
+  show_system: string;
+  show_device: string;
+  show_water_report: string;
+  show_plant_layout: string;
+  show_notification: string;
 }
